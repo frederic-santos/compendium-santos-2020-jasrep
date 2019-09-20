8ac9ec053954577fdfdb87e46fd98d63f8c43947
f.santos <frederic.santos@u-bordeaux.fr>
2019-09-20 13:41:52 +0200

    Update code for Figure1

--- R/Figure1_densityplot.R
+++ R/Figure1_densityplot.R
@@ -1,4 +1,5 @@
-  ## Load the required package:
+  ## Load the required packages:
+  library(anthrostat)
   library(bioanth)
   ## Load the Goldman Data Set:
   data(goldman, package = "bioanth")
@@ -7,17 +8,9 @@
   ## Compute left-right asymmetry in humeral length:
   asym <- na.omit(dat$LHML - dat$RHML)
   names(asym) <- 1:length(asym) # each individual is given a label
-  ## Compute some sample estimates:
-  m <- mean(asym)
-  s <- sd(asym)
-  ## Kernel density estimation:
-  dens <- density(asym)
-  ## Density plot:
+  ## Set graphical parameters:
   par(cex = 1.15, mar = c(4.5, 4.5, 1, 1))
-  plot(dens, main = "")
-  rug(asym, col = "red", lwd = 2)
-  ## Add the names of the two highest values:
-  text(x = sort(asym, dec = TRUE)[1:2], y = 0, pos = 3,
-       labels = names(sort(asym, dec = TRUE)[1:2]), col = "red")
-  ## Add vertical lines (thresholds for outlier detection):
-  abline(v = c(m + 2*s, m - 2*s), col = "blue", lwd = 2, lty = 2)
+  ## Perform outliers detection:
+  id_outl <- norm_outliers(asym, coef = 2)
+  ## Kernel density plot, with decision thresholds for outliers:
+  plot(id_outl, method = "mean_std", number_id = 2)

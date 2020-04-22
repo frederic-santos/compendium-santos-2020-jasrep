bfebb50a42a543b53749f3150e0221d71e2eac81
f-santos <frederic.santos@u-bordeaux.fr>
2020-04-22 14:45:45 +0200

    Update all version numbers of R packages, and R version itself

--- Org_manuscript/auto/manuscript_outliers_Santos_2020.el
+++ Org_manuscript/auto/manuscript_outliers_Santos_2020.el
@@ -5,13 +5,13 @@
                      '(("elsarticle" "review" "3p")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("babel" "english") ("mathabx" "matha" "mathb")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -37,51 +37,51 @@
    (TeX-add-symbols
     "med")
    (LaTeX-add-labels
-    "sec:org93d8cbb"
-    "sec:org208212d"
-    "sec:orgd37d06b"
+    "sec:org60eea6d"
+    "sec:org16f1ad0"
+    "sec:orgf5b90a2"
     "fig:failure2sd"
-    "sec:orge499b89"
+    "sec:org272dae1"
     "eq:formula_loc_scale_univ"
     "tab:comparison_loc_scale_methods"
-    "sec:orgf7bea20"
-    "sec:org94ad6ec"
+    "sec:org467c94f"
+    "sec:org2cd2be0"
     "eq:boxplot"
-    "sec:org195836e"
+    "sec:orgf62579e"
     "eq:adjusted_boxplot"
-    "sec:org2492a45"
+    "sec:org94228f6"
     "fig:asymGiza"
-    "sec:orgfe0833e"
-    "sec:org4118a9d"
+    "sec:orgba10fee"
+    "sec:org51a0f2b"
     "eq:maha"
     "fig:plot3d_Sayala"
     "fig:stripcharts-maha"
-    "sec:orgcfb74af"
+    "sec:org15cb657"
     "fig:anomaly_scores_sayala"
-    "sec:org6d7df67"
+    "sec:orgca385e0"
     "fig:anomaly_scores_dsp2"
     "fig:ddc_dsp2"
-    "sec:org231b70a"
-    "sec:org33b381b"
+    "sec:org6f65ed1"
+    "sec:org51ffa94"
     "fig:type_outliers_reg"
     "fig:robust-corr"
     "fig:robust-regression"
-    "sec:org6bd67fb"
+    "sec:orgfbee468"
     "fig:bagplot"
-    "sec:org23a0e99"
-    "sec:orgff8db48"
-    "sec:orge2ea1e5"
-    "sec:orge0a6950"
+    "sec:orge2f06ff"
+    "sec:org1fdbf6b"
+    "sec:org93d9779"
+    "sec:org2d590e2"
     "appendix:rob-scale-estimate"
-    "sec:orgf86c0a9"
-    "sec:orga464ee2"
+    "sec:org3ab621b"
+    "sec:org9ca9eb5"
     "eq:mad"
-    "sec:org88104e4"
+    "sec:orgddb778d"
     "eq:sn"
-    "sec:orgad0360e"
+    "sec:orgf07dba9"
     "appendix:robust-maha"
     "eq:robust_maha"
-    "sec:org961b26d"
+    "sec:orge52a0a7"
     "appendix:r-packages")
    (LaTeX-add-bibliographies
     "complete_biblio"))
